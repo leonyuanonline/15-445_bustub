8a3212e892124838c67083638e4703732b4fbc26
leonyuan <leonyuan@LAPTOP-7MDO79S9>
2024-08-09 16:48:49 +0800

    1. complete project 1

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -22,9 +22,9 @@ BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager
                                      LogManager *log_manager)
     : pool_size_(pool_size), disk_scheduler_(std::make_unique<DiskScheduler>(disk_manager)), log_manager_(log_manager) {
   // TODO(students): remove this line after you have implemented the buffer pool manager
-  throw NotImplementedException(
-      "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
-      "exception line in `buffer_pool_manager.cpp`.");
+  // throw NotImplementedException(
+  //     "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
+  //     "exception line in `buffer_pool_manager.cpp`.");
 
   // we allocate a consecutive memory space for the buffer pool
   pages_ = new Page[pool_size_];
@@ -38,21 +38,193 @@ BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager
 
 BufferPoolManager::~BufferPoolManager() { delete[] pages_; }
 
-auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * { return nullptr; }
+auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
+  std::lock_guard<std::recursive_mutex> guard(latch_);
+  if (free_list_.empty() && replacer_->Size() == 0) {
+    return nullptr;
+  }
+  frame_id_t frame_id;
+  if (!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Evict(&frame_id)) {
+      return nullptr;
+    }
+
+    if (pages_[frame_id].is_dirty_) {
+      FlushPage(pages_[frame_id].GetPageId());
+    }
+
+    page_table_.erase(pages_[frame_id].GetPageId());
+  }
+  *page_id = AllocatePage();
+  page_table_[*page_id] = frame_id;
+
+  Page &page = pages_[frame_id];
+  page.page_id_ = *page_id;
+  page.is_dirty_ = false;
+  page.pin_count_ = 1;
+  replacer_->SetEvictable(frame_id, false);
+  replacer_->RecordAccess(frame_id);
+  return &page;
+}
 
 auto BufferPoolManager::FetchPage(page_id_t page_id, [[maybe_unused]] AccessType access_type) -> Page * {
-  return nullptr;
+  std::lock_guard<std::recursive_mutex> guard(latch_);
+  if (page_table_.find(page_id) != page_table_.end()) {
+    frame_id_t frame_id = page_table_[page_id];
+    Page &page = pages_[frame_id];
+    page.pin_count_++;
+    replacer_->RecordAccess(frame_id);
+    replacer_->SetEvictable(frame_id, false);
+    return &page;
+  }
+
+  // not int the buffer pool
+  if (free_list_.empty() && replacer_->Size() == 0) {
+    return nullptr;
+  }
+  frame_id_t frame_id;
+  if (!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Evict(&frame_id)) {
+      return nullptr;
+    }
+
+    if (pages_[frame_id].is_dirty_) {
+      FlushPage(pages_[frame_id].GetPageId());
+    }
+
+    page_table_.erase(pages_[frame_id].GetPageId());
+  }
+  auto promise = disk_scheduler_->CreatePromise();
+  auto future = promise.get_future();
+  DiskRequest read_request = {false, pages_[frame_id].GetData(), page_id, std::move(promise)};
+  disk_scheduler_->Schedule(std::move(read_request));
+  future.get();
+
+  // update page info
+  pages_[frame_id].page_id_ = page_id;
+  pages_[frame_id].pin_count_ = 1;
+  pages_[frame_id].is_dirty_ = false;
+
+  page_table_[page_id] = frame_id;
+
+  replacer_->RecordAccess(frame_id);
+  replacer_->SetEvictable(frame_id, false);
+
+  return &pages_[frame_id];
 }
 
+/**
+ * TODO(P1): Add implementation
+ *
+ * @brief Unpin the target page from the buffer pool. If page_id is not in the buffer pool or its pin count is already
+ * 0, return false.
+ *
+ * Decrement the pin count of a page. If the pin count reaches 0, the frame should be evictable by the replacer.
+ * Also, set the dirty flag on the page to indicate if the page was modified.
+ *
+ * @param page_id id of page to be unpinned
+ * @param is_dirty true if the page should be marked as dirty, false otherwise
+ * @param access_type type of access to the page, only needed for leaderboard tests.
+ * @return false if the page is not in the page table or its pin count is <= 0 before this call, true otherwise
+ */
 auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty, [[maybe_unused]] AccessType access_type) -> bool {
-  return false;
+  std::lock_guard<std::recursive_mutex> guard(latch_);
+
+  if (page_table_.find(page_id) == page_table_.end()) {
+    return false;
+  }
+
+  frame_id_t frame_id = page_table_[page_id];
+  Page &page = pages_[frame_id];
+
+  if (is_dirty) {
+    page.is_dirty_ = true;
+  }
+
+  if (page.pin_count_ > 0) {
+    page.pin_count_--;
+  }
+
+  if (page.pin_count_ == 0) {
+    replacer_->SetEvictable(frame_id, true);
+  }
+  return true;
 }
 
-auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool { return false; }
+auto BufferPoolManager::FlushPage(page_id_t page_id) -> bool {
+  std::lock_guard<std::recursive_mutex> guard(latch_);
 
-void BufferPoolManager::FlushAllPages() {}
+  if (page_table_.find(page_id) == page_table_.end()) {
+    return false;
+  }
 
-auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool { return false; }
+  frame_id_t frame_id = page_table_[page_id];
+  Page &page = pages_[frame_id];
+  // std::cout << "flush page " << page_id << std::endl;
+  auto promise = disk_scheduler_->CreatePromise();
+  auto future = promise.get_future();
+
+  DiskRequest write_r = {true, page.GetData(), page_id, std::move(promise)};
+  disk_scheduler_->Schedule(std::move(write_r));
+  future.get();
+  // std::cout << "flush page " << page_id << " ok" << std::endl;
+  page.is_dirty_ = false;
+  return true;
+}
+
+void BufferPoolManager::FlushAllPages() {
+  for (const auto &[page_id, frame_id] : page_table_) {
+    FlushPage(page_id);
+  }
+}
+
+/**
+ * TODO(P1): Add implementation
+ *
+ * @brief Delete a page from the buffer pool. If page_id is not in the buffer pool, do nothing and return true. If the
+ * page is pinned and cannot be deleted, return false immediately.
+ *
+ * After deleting the page from the page table, stop tracking the frame in the replacer and add the frame
+ * back to the free list. Also, reset the page's memory and metadata. Finally, you should call DeallocatePage() to
+ * imitate freeing the page on the disk.
+ *
+ * @param page_id id of page to be deleted
+ * @return false if the page exists but could not be deleted, true if the page didn't exist or deletion succeeded
+ */
+auto BufferPoolManager::DeletePage(page_id_t page_id) -> bool {
+  std::lock_guard<std::recursive_mutex> guard(latch_);
+
+  if (page_table_.find(page_id) == page_table_.end()) {
+    return true;
+  }
+
+  frame_id_t frame_id = page_table_[page_id];
+  Page &page = pages_[frame_id];
+
+  if (page.pin_count_ > 0) {
+    return false;
+  }
+
+  replacer_->SetEvictable(frame_id, false);
+  if (page.is_dirty_) {
+    FlushPage(pages_[frame_id].GetPageId());
+  }
+
+  page_table_.erase(page_id);
+  pages_[frame_id].page_id_ = INVALID_PAGE_ID;
+  pages_[frame_id].ResetMemory();
+  free_list_.emplace_back(frame_id);
+
+  DeallocatePage(page_id);
+
+  return true;
+}
 
 auto BufferPoolManager::AllocatePage() -> page_id_t { return next_page_id_++; }
 
--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -15,16 +15,118 @@
 
 namespace bustub {
 
+LRUKNode::LRUKNode() : k_(0), fid_(0) {}
+LRUKNode::LRUKNode(size_t k, frame_id_t fid) : k_(k), fid_(fid) {}
+void LRUKNode::RecordAccess(size_t timestamp) {
+  if (history_.size() >= k_) {
+    history_.pop_front();
+  }
+  history_.push_back(timestamp);
+}
+auto LRUKNode::GetKDistance(size_t cur_timestamp) const -> size_t {
+  if (history_.size() < k_) {
+    return std::numeric_limits<size_t>::max();
+  }
+  return cur_timestamp - history_.front();
+}
+auto LRUKNode::GetEarliestTimestamp() const -> size_t {
+  if (history_.empty()) {
+    return std::numeric_limits<size_t>::max();
+  }
+  return history_.front();
+}
+
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+// Successful eviction of a frame should decrement the size of replacer and remove the frame's
+// access history.
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::lock_guard<std::mutex> latch(latch_);
+  if (curr_size_ == 0) {
+    return false;
+  }
+  size_t max_k_distance = 0;
+  frame_id_t victim_id = -1;
+  size_t earliest_timestamp = std::numeric_limits<size_t>::max();
+  frame_id_t lru_victim_id = -1;
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+  for (const auto &entry : node_store_) {
+    const auto &node = entry.second;
+    if (node.IsEvictable()) {
+      size_t k_distance = node.GetKDistance(current_timestamp_);
+      if (k_distance == std::numeric_limits<size_t>::max()) {
+        size_t timestamp = node.GetEarliestTimestamp();
+        if (timestamp < earliest_timestamp) {
+          earliest_timestamp = timestamp;
+          lru_victim_id = entry.first;
+        }
+      } else if (k_distance > max_k_distance) {
+        max_k_distance = k_distance;
+        victim_id = entry.first;
+      }
+    }
+  }
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+  if (lru_victim_id != -1) {
+    *frame_id = lru_victim_id;
+  } else if (victim_id != -1) {
+    *frame_id = victim_id;
+  } else {
+    return false;
+  }
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+  node_store_.erase(*frame_id);
+  curr_size_--;
+  return true;
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, AccessType access_type) {
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    throw std::out_of_range("Invalid frame id");
+  }
+  std::lock_guard<std::mutex> latch(latch_);
+  if (node_store_.find(frame_id) == node_store_.end()) {
+    node_store_.emplace(frame_id, LRUKNode(k_, frame_id));
+  }
+  node_store_[frame_id].RecordAccess(current_timestamp_);
+  current_timestamp_++;
+}
+
+// Note that size is equal to number of evictable entries.
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    throw std::out_of_range("Invalid frame id");
+  }
+  std::lock_guard<std::mutex> latch(latch_);
+  if (node_store_.find(frame_id) == node_store_.end()) {
+    node_store_.emplace(frame_id, LRUKNode(k_, frame_id));
+  }
+  auto &node = node_store_[frame_id];
+  if (node.IsEvictable() != set_evictable) {
+    node.SetEvictable(set_evictable);
+    curr_size_ += set_evictable ? 1 : -1;
+  }
+}
+
+// This function should also decrement replacer's size if removal is successful.
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  if (static_cast<size_t>(frame_id) >= replacer_size_) {
+    throw std::out_of_range("Invalid frame id");
+  }
+  std::lock_guard<std::mutex> latch(latch_);
+  if (node_store_.find(frame_id) == node_store_.end()) {
+    return;
+  }
+  if (!node_store_.at(frame_id).IsEvictable()) {
+    throw std::logic_error("Cannot remove non-evictable frame");
+  }
+  node_store_.erase(frame_id);
+  curr_size_--;
+}
+
+auto LRUKReplacer::Size() -> size_t {
+  std::lock_guard<std::mutex> latch(latch_);
+  return curr_size_;
+}
 
 }  // namespace bustub
